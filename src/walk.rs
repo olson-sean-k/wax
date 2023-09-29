13a3e160fe89540520b201231dbcd8ae8fd8b1ab
Sean Olson <olson.sean.k@gmail.com>
2023-09-29 14:36:42 -0700

    Rename `Compose` to `Combine`.
    This change renames the `Compose` trait to `Combine` and uses the
    "combine" nomenclature when discussing combinators (`Any`). Some
    documentation and some identifiers concerning `Any` have also been
    changed to reduce ambiguity and to better relfect these names.

--- src/walk.rs
+++ src/walk.rs
@@ -12,7 +12,7 @@ use walkdir::{self, DirEntry, WalkDir};
 use crate::capture::MatchedText;
 use crate::encode::CompileError;
 use crate::token::{self, Token, TokenTree};
-use crate::{BuildError, CandidatePath, Compose, Glob};
+use crate::{BuildError, CandidatePath, Combine, Glob};
 
 pub type WalkItem<'e> = Result<WalkEntry<'e>, WalkError>;
 
@@ -344,24 +344,25 @@ pub struct Negation {
 }
 
 impl Negation {
-    /// Composes glob expressions into a `Negation`.
+    /// Combines glob expressions into a `Negation`.
     ///
-    /// This function accepts an [`IntoIterator`] with items that implement the
-    /// [`Compose`] trait such as [`Glob`] and `&str`.
+    /// This function accepts an [`IntoIterator`] with items that implement
+    /// [`Combine`], such as [`Glob`] and `&str`.
     ///
     /// # Errors
     ///
     /// Returns an error if any of the inputs fail to build. If the inputs are a
-    /// compiled [`Pattern`] type such as [`Glob`], then this only occurs if the
-    /// compiled program is too large.
+    /// compiled [`Pattern`] types such as [`Glob`], then this only occurs if
+    /// the compiled program is too large.
     ///
+    /// [`Combine`]: crate::Combine
     /// [`Glob`]: crate::Glob
-    /// [`Pattern`]: crate::Pattern
     /// [`IntoIterator`]: std::iter::IntoIterator
+    /// [`Pattern`]: crate::Pattern
     pub fn any<'t, I>(patterns: I) -> Result<Self, BuildError>
     where
         I: IntoIterator,
-        I::Item: Compose<'t>,
+        I::Item: Combine<'t>,
     {
         let (exhaustive, nonexhaustive) = patterns
             .into_iter()
@@ -667,7 +668,7 @@ impl<'g> Walk<'g> {
     pub fn not<'t, I>(self, patterns: I) -> Result<impl 'g + FileIterator, BuildError>
     where
         I: IntoIterator,
-        I::Item: Compose<'t>,
+        I::Item: Combine<'t>,
     {
         Negation::any(patterns)
             .map(|negation| self.filter_tree(move |entry| negation.target(entry)))
